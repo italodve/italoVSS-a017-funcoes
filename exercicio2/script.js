ef1e414d965aa6791075449209ac9da73f76faa8
italodve <italovinicius@Gmail.com>
2022-09-14 15:16:18 -0300

    terminado

---
+++ exercicio2/script.js
@@ -0,0 +1,38 @@
+function soma(n1,n2){
+    return n1+n2
+    }
+    const somados =soma(8,9)
+    console.log(somados)
+
+
+
+
+
+
+function numeros(num1,num2){
+    if (num1 >= num2) {
+        console.log("o primeiro Maior ou igual")
+    } else {
+        console.log("o segundo valor é maior ou igual")
+    }
+}
+numeros(10,9) 
+
+
+
+function pares(number) {
+if(number %2 === 0){
+console.log("é par")
+
+}else {
+    console.log("não é par")
+}
+
+}pares(10) 
+
+
+function texto(caracter){
+    console.log(caracter.length)
+    console.log(caracter.toUpperCase())
+}
+texto("vamo ver se funciona")
---
+++ exercicio3/script.js
@@ -0,0 +1,32 @@
+
+let n1 = Number(prompt("digite o n1"))
+let n2 = Number(prompt("digite o n2"))
+
+
+function f1 () {
+console.log(n1 + n2)
+}
+
+function f2 () {
+    console.log(n1 - n2)
+    } 
+
+
+    
+function f3 () {
+    console.log(n1 / n2)
+    } 
+
+
+
+    
+function f4 () {
+    console.log(n1*n2)
+    }
+
+
+
+f1() 
+f2()
+f3() 
+f4()
